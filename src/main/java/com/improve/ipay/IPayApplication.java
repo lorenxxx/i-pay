9183bdb0c5df3910a6d3c081652d3c4e9c1f9c6e
liangliang <liangliang@icourt.cc>
2018-08-21 23:07:58 +0800

    First Commit

---
+++ src/main/java/com/improve/ipay/IPayApplication.java
@@ -0,0 +1,12 @@
+package com.improve.ipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IPayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/improve/ipay/IPayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.improve.ipay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IPayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
